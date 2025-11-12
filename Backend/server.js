7b5376d7cc4f905156a51d2b5a4c0ab98121205b
Ge Zheng <gzheng12@my.bcit.ca>
2025-11-12 10:53:35 -0800

    Add render config

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 app.use(cors());
 app.use(express.static("../Frontend"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`The server up and it is listening to ${PORT}`);
